7f9f07931256ad046758ebce6d408bbe72e3b4ba
HYUCK7 <iqeq12191219@gmail.com>
2022-02-08 20:45:46 +0900

    2022-02-08 20:45 구구단표

--- java/com/example/demo/quiz/service/Feb08ServiceImpl.java
+++ java/com/example/demo/quiz/service/Feb08ServiceImpl.java
@@ -37,13 +37,12 @@ public class Feb08ServiceImpl implements Feb08Service {
 
     @Override
     public void gugudan(Scanner scanner) {
-        System.out.println("5. 구구단" + "\n" + "   *** 몇 단을 출력할까요 ***    ");
-        String result = "";
-        int i = scanner.nextInt();
-        System.out.println("    ###    " + i + "단    ###    ");
-        for (int j = 1; j < 10; j++) {
-            result = String.format("%d x %d = %d", i, j, (i * j));
-            System.out.print(result + "\t");
+        System.out.println("5. 구구단");
+        for (int i = 1; i < 10; i++) {
+            System.out.println(" ### " + i + "단 ###");
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
         }
     }
 }
