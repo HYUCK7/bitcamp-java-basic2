9060c20a20740360d8395ec26e1f488736e80939
HYUCK7 <iqeq12191219@gmail.com>
2022-02-09 09:30:05 +0900

    2022-02-09 09:29 구구단 최종

--- java/com/example/demo/quiz/service/Feb08ServiceImpl.java
+++ java/com/example/demo/quiz/service/Feb08ServiceImpl.java
@@ -42,6 +42,15 @@ public class Feb08ServiceImpl implements Feb08Service {
             System.out.println(" ### " + i + "단 ###");
             for (int j = 1; j < 10; j++) {
                 System.out.println(i + " x " + j + " = " + i * j);
+                /**
+                 *  System.out.println("5. 구구단" + "\n" + "   *** 몇 단을 출력할까요 ***    ");
+                 *         String result = "";
+                 *         int i = scanner.nextInt();
+                 *         System.out.println("    ###    " + i + "단    ###    ");
+                 *         for (int j = 1; j < 10; j++) {
+                 *             result = String.format("%d x %d = %d", i, j, (i * j));
+                 *             System.out.print(result + "\t");
+                 */
             }
         }
     }
